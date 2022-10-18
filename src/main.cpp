10f157100711f47f966ca5e4ea2a7699b0052b9b
dryerem19 <mamayma8@gmail.com>
2022-10-18 14:36:54 +0300

    Конфигурация для сборки на linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,59 @@
 ////#include "pch/pch.h"
 //
 
-//#include "Window.h"
-//#include "KitEnginePch.h"
+#include "Window.h"
+#include "KitEnginePch.h"
 
 #include <iostream>
 
-#include <imgui.h>
-//#include <imgui_impl_glfw.h>
-//#include <imgui_impl_opengl3.h>
-
 int main(void)
 {
-    std::cout << "ПИДАРАС!!";
+    KitEngine::WindowProps props;
+    props.Title = "KitEngine";
+    props.WindowClass = "KitEngineWindowClass";
+    props.Width = 800;
+    props.Height = 600;
 
-//    KitEngine::WindowProps props;
-//    props.Title = "KitEngine";
-//    props.WindowClass = "KitEngineWindowClass";
-//    props.Width = 800;
-//    props.Height = 600;
-//
-//    KitEngine::Window window(props);
-//    if (!window.Initialize()) {
-//        return -1;
-//    }
-//
+    KitEngine::Window window(props);
+    if (!window.Initialize()) {
+        return -1;
+    }
+
     // Setup Dear ImGui context
-    //git IMGUI_CHECKVERSION();
-//
-//    ImGui::CreateContext();
-//    ImGuiIO& io = ImGui::GetIO();
-//    io.Fonts->AddFontFromFileTTF("../fonts/Roboto-Bold.ttf", 14, NULL, io.Fonts->GetGlyphRangesCyrillic());
-//    // Setup Dear ImGui style
-//    ImGui::StyleColorsDark();
-//
-//    // Setup Platform/Renderer backends
-//    ImGui_ImplGlfw_InitForOpenGL(window.GetWindowPointer(), true);
-//    ImGui_ImplOpenGL3_Init("#version 130");
-//
-//    while (window.Exec()) {
-//        window.Update();
-//
-//        /* Render here */
-//        glClearColor(1,1,1,1);
-//        glClear(GL_COLOR_BUFFER_BIT);
-//
-//        // Start the Dear ImGui frame
-//        ImGui_ImplOpenGL3_NewFrame();
-//        ImGui_ImplGlfw_NewFrame();
-//        ImGui::NewFrame();
-//
-//        // Show demo window
-//        ImGui::Begin("Window");
-//        ImGui::Text("Как дела ?");
-//        ImGui::End();
-//
-//        ImGui::Render();
-//        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-//
-//        window.SwapBuffers();
-//    }
+    IMGUI_CHECKVERSION();
+
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.Fonts->AddFontFromFileTTF("../fonts/Roboto-Bold.ttf", 14, NULL, io.Fonts->GetGlyphRangesCyrillic());
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window.GetWindowPointer(), true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    while (window.Exec()) {
+        window.Update();
+
+        /* Render here */
+        glClearColor(1,1,1,1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Show demo window
+        ImGui::Begin("Window");
+        ImGui::Text("Как дела ?");
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        window.SwapBuffers();
+    }
 
     return 0;
 }
