704d7bfccbbb8e1a6edfbba812c390bb3d37bdc8
Evgeniy <dubasov.eugeni@yandex.ru>
2022-10-26 00:21:16 +0300

    fix_texture_show

--- res/shaders/glsl/transform_test.glsl
+++ res/shaders/glsl/transform_test.glsl
@@ -2,12 +2,17 @@
 #version 410 core
 
 layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 texcoord;
 
 uniform mat4 uTransform;
+
+
+out vec2 vTexcoord;
 
 void main()
 {
     gl_Position = uTransform * vec4(position, 1.0f);
+    vTexcoord = texcoord;
 }
 
 #shader fragment
@@ -15,9 +20,12 @@ void main()
 
 layout(location = 0) out vec4 color;
 
-uniform vec4 uColor;
+uniform sampler2D uTexture;
+
+in vec2 vTexcoord;
 
 void main()
 {
-    color = uColor;
+    vec4 texcolor = texture(uTexture, vTexcoord);
+    color = texcolor;
 }
