5e9566ddad21ca2cd00322a8587fec42058f5947
Evgeniy <dubasov.eugeni@yandex.ru>
2022-10-28 23:43:38 +0300

    create_camera_and_movement

--- LevelEditor/src/main.cpp
+++ LevelEditor/src/main.cpp
@@ -22,6 +22,20 @@
 
 using namespace KitEngine::Graphics;
 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouse_callback(GLFWwindow* window, int key, int action, int mode);
+void mouse_movement(GLFWwindow* window, double xpos, double ypos);
+void do_movement();
+
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+GLfloat yaw   = -90.0f;	// Yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right (due to how Eular angles work) so we initially rotate a bit to the left.
+GLfloat pitch =   0.0f;
+GLfloat lastX =  800  / 2.0;
+GLfloat lastY =  600 / 2.0;
+bool keys[1024];
+
 int main(void)
 {
     KitEngine::WindowProps props;
@@ -35,9 +49,18 @@ int main(void)
         return -1;
     }
 
+
+    glfwSetKeyCallback(window.GetWindowPointer(), key_callback);
+    glfwSetMouseButtonCallback(window.GetWindowPointer(), mouse_callback);
+    glfwSetCursorPosCallback(window.GetWindowPointer(), mouse_movement);
+
+    glfwSetInputMode(window.GetWindowPointer(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+
     KitEngine::Core::Log::Info("Самый лучший движок в мире!!");
     KitEngine::Core::Log::Warning("Привет, {} dsdsdsds {}!", "Женя", "Вася");
     KitEngine::Core::Log::Critical("Хьюстон, у нас проблемы :(");
+
+
 
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
@@ -92,11 +115,21 @@ int main(void)
     Renderer renderer;
 
     glm::mat4 transform = glm::mat4(1.0f);
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 projection = glm::mat4(1.0f);
     while (window.Exec()) {
         window.Update();
+        do_movement();
+
 
         /* Render here */
         renderer.Clear();
+
+        // Camera
+        view = glm::lookAt(cameraPos,cameraPos + cameraFront, cameraUp);
+        projection = glm::perspective(45.0f, (GLfloat)props.Width / (GLfloat)props.Height, 0.1f, 100.0f);
+        shader.SetUniformMatrix4fv("uView", 1, GL_FALSE, glm::value_ptr(view));
+        shader.SetUniformMatrix4fv("uProjection", 1, GL_FALSE, glm::value_ptr(projection));
 
         transform = glm::rotate(transform, 0.02f, glm::vec3(0.0f, 0.0f, 1.0f));
         shader.SetUniformMatrix4fv("uTransform",1, GL_FALSE,
@@ -122,4 +155,92 @@ int main(void)
     }
 
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+            keys[key] = true;
+        else if (action == GLFW_RELEASE)
+            keys[key] = false;
+    }
+}
+
+void mouse_callback(GLFWwindow* window, int key, int action, int mode)
+{
+
+    if(action == GLFW_PRESS)
+    {
+        keys[key] = true;
+    }
+    else if (action == GLFW_RELEASE)
+    {
+        keys[key] = false;
+    }
+
+}
+
+void mouse_movement(GLFWwindow* window, double xpos, double ypos)
+{
+    if(keys[GLFW_MOUSE_BUTTON_LEFT])
+    {
+
+
+        GLfloat xoffset = xpos - lastX;
+        GLfloat yoffset = lastY - ypos;
+        lastX = xpos;
+        lastY = ypos;
+
+
+        GLfloat sensitivity = 0.05;
+        xoffset *= sensitivity;
+        yoffset *= sensitivity;
+
+        yaw   += xoffset;
+        pitch += yoffset;
+
+        if(pitch > 89.0f)
+            pitch = 89.0f;
+        if(pitch < -89.0f)
+            pitch = -89.0f;
+
+        glm::vec3 front;
+        front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+        front.y = sin(glm::radians(pitch));
+        front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+        cameraFront = glm::normalize(front);
+
+    }
+
+
+}
+
+void do_movement()
+{
+    // Camera controls
+    GLfloat cameraSpeed = 0.30f;
+    if (keys[GLFW_KEY_W])
+    {
+        cameraPos += cameraSpeed * cameraFront;
+
+    }
+    if (keys[GLFW_KEY_S])
+    {
+        cameraPos -= cameraSpeed * cameraFront;
+
+    }
+    if (keys[GLFW_KEY_A])
+    {
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+    }
+    if (keys[GLFW_KEY_D])
+    {
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+
 }
