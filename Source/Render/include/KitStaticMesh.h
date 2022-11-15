93b35d628f073bd31cd0fc81d1cb867f83e3b75a
dryerem19 <mamayma8@gmail.com>
2022-11-15 15:46:00 +0300

    add KitStaticMesh

--- Source/Render/include/KitStaticMesh.h
+++ Source/Render/include/KitStaticMesh.h
@@ -1,10 +1,28 @@
 #pragma once
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "KitVertex.h"
 
 namespace Render
 {
     class KitStaticMesh
     {
     public:
-        
+        VertexArray  mVertexArray;
+        VertexBuffer mVertexBuffer;
+        IndexBuffer  mIndexBuffer;
+
+        KitStaticMesh(const std::vector<KitVertex>& vertices,
+                      const std::vector<uint32_t>&  indices)
+            : mVertexBuffer(vertices.data(), vertices.size()),
+              mIndexBuffer(indices.data(), indices.size())
+            {
+                VertexBufferLayout bufferLayout;
+                bufferLayout.AddFloatElement(3);
+                bufferLayout.AddFloatElement(2);
+                bufferLayout.AddFloatElement(3);
+
+                mVertexArray.AddBuffer(mVertexBuffer, bufferLayout);
+            }
     };
 }
