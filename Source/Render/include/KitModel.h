ebde9964d2115707b9fcf9e81dfb632657354cdb
dryerem19 <mamayma8@gmail.com>
2022-11-16 22:16:51 +0300

    create KitModel

---
+++ Source/Render/include/KitModel.h
@@ -0,0 +1,100 @@
+#pragma once
+#include "KitStaticMesh.h"
+#include "KitMaterial.h"
+#include "KitVertex.h"
+
+namespace Render
+{
+    class KitModel
+    {
+    private:
+        // @brief Список мешей модели
+        std::vector<std::shared_ptr<KitStaticMesh>> mChildren;
+
+    public:
+        // @brief Конструктор по умолчанию
+        KitModel() = default;
+
+        // @brief Конструктор копирования по умолчанию
+        KitModel(const KitModel&) = default;
+
+        // @brief Деструктор по умолчанию
+        ~KitModel() = default;
+
+        /*
+        @brief Конструктор импорта модели с помощью Assimp
+        */
+        KitModel(const std::string& filepath);
+
+        /*
+        @brief Метод инициализация меша 
+        @param filepath Путь к модели на диске для импорта 
+        */
+        void Init(const std::string& filepath);
+
+        /*
+        @brief Итератор
+        @return Итератор на первый элемент
+        */
+        std::vector<std::shared_ptr<Render::KitStaticMesh>>::iterator begin() 
+        { return mChildren.begin(); }
+
+        /*
+        @brief Итератор
+        @return Итератор на последний элемент
+        */
+        std::vector<std::shared_ptr<Render::KitStaticMesh>>::iterator end() 
+        { return mChildren.end(); }
+
+        /*
+        @brief Константный итератор
+        @return Итератор на последний элемент
+        */
+        std::vector<std::shared_ptr<Render::KitStaticMesh>>::const_iterator cbegin() 
+        { return mChildren.cbegin(); }
+
+        /*
+        @brief Константный итератор
+        @return Итератор на последний элемент
+        */
+        std::vector<std::shared_ptr<Render::KitStaticMesh>>::const_iterator cend() 
+        { return mChildren.cend(); }        
+
+    private:
+
+        /*
+        @brief Рекурсивная обработка нод сцены assimp'а 
+        @param pNode Указатель на ноду
+        @param pScene Указатель на сцену
+        @param filepath Путь к файлу модели
+        */
+        void ProcessAssimpNode(const aiNode* pNode, const aiScene* pScene, const std::string& filepath);
+
+        /*
+        @brief Обработка меша assimp'а
+        @param pMesh Указатель на меш assimp'а
+        @param pScene Указатель на сцену
+        @param filepath Путь к файлу модели
+        @return Экземпляр класса KitStaticMesh
+        */
+        std::shared_ptr<KitStaticMesh> ProcessAssimpMesh(const aiMesh* pMesh, const aiScene* pScene, const std::string& filepath);
+
+        /*
+        @brief Обработка материала assimp'а
+        @param pMaterial Указатель на материал assimp'а
+        @param directory Директория с моделью
+        @return Экземпляр класса KitMaterial
+        */
+        void ProcessAssimpMaterial(const aiMaterial* pMaterial, const std::string& directory, KitMaterial& kitMaterial);
+
+        /*
+        @brief Загрузка текстур
+        @param pMaterial Указатель на материал assimp'а
+        @param type Тип текстур, которые нужно загрузить
+        @param directory Директория с моделью
+        @return Список текстур std::vector<KitTexture>
+        */
+        std::vector<std::shared_ptr<KitTexture>> LoadMaterialTextures(const aiMaterial* pMaterial, aiTextureType type, 
+            const std::string& directory);
+    };
+}
--- Source/Render/include/KitStaticMesh.h
+++ Source/Render/include/KitStaticMesh.h
@@ -14,10 +14,7 @@ namespace Render
         VertexBuffer mVertexBuffer;
         IndexBuffer  mIndexBuffer;
         KitTransform mTransform;
-
-        // Дочерние меши
-        std::vector<std::shared_ptr<KitStaticMesh>> mChildren;
-
+        
         // Материал меша
         KitMaterial mMaterial;
 
@@ -45,63 +42,9 @@ namespace Render
                       const std::vector<uint32_t>&  indices, const KitMaterial& material);
 
         /*
-        @param filepath Путь к модели на диске для импорта 
-        */
-        KitStaticMesh(const std::string& filepath);
-
-        /*
         @brief Метод инициализация меша 
         @param filepath Путь к модели на диске для импорта 
         */
         void Init(const std::vector<KitVertex>& vertices, const std::vector<uint32_t>&  indices);
-
-        /*
-        @brief Метод инициализация меша 
-        @param filepath Путь к модели на диске для импорта 
-        */
-        void Init(const std::string& filepath);
-
-        std::vector<std::shared_ptr<Render::KitStaticMesh>>::iterator begin() { return mChildren.begin(); }
-        std::vector<std::shared_ptr<Render::KitStaticMesh>>::iterator end() { return mChildren.end(); }
-        std::vector<std::shared_ptr<Render::KitStaticMesh>>::const_iterator cbegin() { return mChildren.cbegin(); }
-        std::vector<std::shared_ptr<Render::KitStaticMesh>>::const_iterator cend() { return mChildren.cend(); }
-
-
-    private:
-
-        /*
-        @brief Рекурсивная обработка нод сцены assimp'а 
-        @param pNode Указатель на ноду
-        @param pScene Указатель на сцену
-        @param filepath Путь к файлу модели
-        */
-        void ProcessAssimpNode(const aiNode* pNode, const aiScene* pScene, const std::string& filepath);
-
-        /*
-        @brief Обработка меша assimp'а
-        @param pMesh Указатель на меш assimp'а
-        @param pScene Указатель на сцену
-        @param filepath Путь к файлу модели
-        @return Экземпляр класса KitStaticMesh
-        */
-        std::shared_ptr<KitStaticMesh> ProcessAssimpMesh(const aiMesh* pMesh, const aiScene* pScene, const std::string& filepath);
-
-        /*
-        @brief Обработка материала assimp'а
-        @param pMaterial Указатель на материал assimp'а
-        @param directory Директория с моделью
-        @return Экземпляр класса KitMaterial
-        */
-        void ProcessAssimpMaterial(const aiMaterial* pMaterial, const std::string& directory, KitMaterial& kitMaterial);
-
-        /*
-        @brief Загрузка текстур
-        @param pMaterial Указатель на материал assimp'а
-        @param type Тип текстур, которые нужно загрузить
-        @param directory Директория с моделью
-        @return Список текстур std::vector<KitTexture>
-        */
-        std::vector<std::shared_ptr<KitTexture>> LoadMaterialTextures(const aiMaterial* pMaterial, aiTextureType type, 
-            const std::string& directory);
     };
 }
