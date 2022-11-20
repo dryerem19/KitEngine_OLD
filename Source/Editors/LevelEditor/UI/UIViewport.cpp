91ea4a2ea38b600937f3457112d2012aab95ac85
dryerem <mamayma8@gmail.com>
2022-11-21 01:56:00 +0300

    fix gizmo

--- Source/Editors/LevelEditor/UI/UIViewport.cpp
+++ Source/Editors/LevelEditor/UI/UIViewport.cpp
@@ -30,13 +30,12 @@ namespace LevelEditor
         
         if (uiSceneTree->mSelectedObject && mOperation != GizmoOperation::NONE)
         {
-            ImGuizmo::OPERATION op = (ImGuizmo::OPERATION)mOperation;
             auto& transform = uiSceneTree->mSelectedObject.GetComponent<Render::KitTransform>();
 
             /* Build transform matrix */
             float transformMatrix[16];
             ImGuizmo::RecomposeMatrixFromComponents(&transform.Translation.x, &transform.Rotation.x, &transform.Scale.x, transformMatrix);
-            ImGuizmo::Manipulate(EditorCamera::Instance().GetView(), EditorCamera::Instance().GetPerspective(), uiTopBarTools->mode, ImGuizmo::MODE::LOCAL, transformMatrix);
+            ImGuizmo::Manipulate(EditorCamera::Instance().GetView(), EditorCamera::Instance().GetPerspective(), (ImGuizmo::OPERATION)mOperation, ImGuizmo::MODE::LOCAL, transformMatrix);
             
             /* If we moved the manipulator */
             if (ImGuizmo::IsUsing())
@@ -45,18 +44,18 @@ namespace LevelEditor
                 float translation[3], rotation[3], scale[3];
                 ImGuizmo::DecomposeMatrixToComponents(transformMatrix, translation, rotation, scale);
 
-                if (op == ImGuizmo::OPERATION::TRANSLATE)
+                switch (mOperation)
                 {
+                case GizmoOperation::TRANSLATE:
                     transform.Translation = glm::vec3(translation[0], translation[1], translation[2]);
-                }
-                else if (op == ImGuizmo::OPERATION::ROTATE)
-                {
+                    break;
+                case GizmoOperation::ROTATE:
                     glm::vec3 deltaRotation = glm::vec3(rotation[0], rotation[1], rotation[2]) - transform.Rotation;
                     transform.Rotation += deltaRotation;
-                }
-                else if (op == ImGuizmo::OPERATION::SCALE)
-                {
+                case GizmoOperation::SCALE:
                     transform.Scale = glm::vec3(scale[0], scale[1], scale[2]);
+                default:
+                    break;
                 }
             }
         }
